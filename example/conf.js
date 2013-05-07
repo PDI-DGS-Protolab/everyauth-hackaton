6f5c20e0f66c5f876a642d04893ce59265a81587
Javier Mendiara <jmendiara@gmail.com>
2013-05-07 19:22:17 +0300

    Update conf.js

--- example/conf.js
+++ example/conf.js
@@ -1,5 +1,9 @@
 module.exports = {
-    fb: {
+    movistar: {
+        consumerKey: 'ONa0kd4CsYftGGYgfCjYXn94sLrZAA8S',
+        consumerSecret: '9hwqAcMMHltPOb7h'
+    }
+  , fb: {
         appId: '111565172259433'
       , appSecret: '85f7e0a0cc804886180b887c1f04a3c1'
     }
